6094f5789d6500985fdcde0772fea32b6013ff48
woohyuk Park <psj4415@naver.com>
2023-12-06 19:22:38 +0900

    second project

--- app.py
+++ app.py
@@ -32,5 +32,24 @@ with app.app_context():
 def home():
     return render_template("index.html")
 
+# 회원가입 데이터 양식 받기.
+@app.route('/api/user', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['user_name']
+        userid = request.form['user_id']
+        password = request.form['user_password']
+
+        # 사용자가 이미 존재하는지 확인
+        existing_user = User.query.filter_by(userName=username).first()
+        if existing_user:
+            return '이미 존재하는 사용자입니다!'
+        
+        # 새로운 사용자 생성
+        new_user = User(userName=username, userId=userid, password=password)
+
+        # 데이터베이스에 추가
+        db.session.add(new_user)
+        db.session.commit()
 if __name__ == "__main__":
     app.run(debug=True)
