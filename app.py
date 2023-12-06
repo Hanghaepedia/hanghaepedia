da4d819589bb767a6ec711cdd9f619b654ab0481
곽민지 <mini0006@naver.com>
2023-12-06 14:52:41 +0900

    데이터베이스 생성

--- app.py
+++ app.py
@@ -10,14 +10,23 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
-
 db = SQLAlchemy(app)
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     password = db.Column(db.String(100), nullable=False)
-#     with app.app_context():
-#         db.create_all()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(100), nullable=False)
+    userId = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+def __init__(self, userName, userId, password):
+    self.userName = userName
+    self.userId = userId
+    self.password = password
+def __repr__(self):
+    return "<TbTest('%d', '%s', '%s'>" %(self.id, self.userName, self.userId, self.password)
+    
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
